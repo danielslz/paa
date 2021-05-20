798b71b04fc128dadb59306928cd2603f46aa89f
Daniel Lima <danielslz@gmail.com>
2021-05-20 09:22:11 -0300

    Add disconnected graphs.

--- prim/mst_prim.py
+++ prim/mst_prim.py
@@ -8,7 +8,7 @@ from random import randint
 
 
 class Heap():
-    # data format: [node_index, min_weight, parent]
+    # data format: [min_weight, node_index, parent]
     heap = []
 
     def init(self, initial):
@@ -44,7 +44,7 @@ class Heap():
         return None
 
 
-def build_graph(num_nodes):
+def build_graph(num_nodes, disconnected=False):
     num_edges = randint(num_nodes, num_nodes * 3)
     top_weight = randint(num_nodes//4, num_nodes//2)
 
@@ -53,13 +53,38 @@ def build_graph(num_nodes):
     for i in range(num_nodes):
         G.add_node(i+1)
     # add edges
-    for i in range(num_edges):
-        u = randint(1, num_nodes)
-        v = randint(1, num_nodes)
-        while u == v:
+    if disconnected:
+        break_node = randint(1, num_nodes)
+        edges_tree_1 = num_edges // break_node
+        edges_tree_2 = num_edges - edges_tree_1
+        temp_1 = min(edges_tree_1, edges_tree_2)
+        temp_2 = max(edges_tree_1, edges_tree_2)
+        if break_node < num_nodes // 2:
+            edges_tree_1 = temp_1
+            edges_tree_2 = temp_2
+        else:
+            edges_tree_1 = temp_2
+            edges_tree_2 = temp_1
+        # build subgraph 1
+        for i in range(edges_tree_1):
+            u = randint(1, break_node)
+            v = randint(1, break_node)
+            weight = randint(1, top_weight)
+            G.add_edge(u, v, weight=weight, color='k')
+        # build subgraph 2
+        for i in range(edges_tree_2):
+            u = randint(break_node + 1, num_nodes)
+            v = randint(break_node + 1, num_nodes)
+            weight = randint(1, top_weight)
+            G.add_edge(u, v, weight=weight, color='k')
+    else:
+        for i in range(num_edges):
+            u = randint(1, num_nodes)
             v = randint(1, num_nodes)
-        weight = randint(1, top_weight)
-        G.add_edge(u, v, weight=weight, color='k')
+            while u == v:
+                v = randint(1, num_nodes)
+            weight = randint(1, top_weight)
+            G.add_edge(u, v, weight=weight, color='k')
     
     return G
 
@@ -115,15 +140,15 @@ def mst_weight(mst):
 
 
 # build graph
-g = build_graph(10)
+g = build_graph(10, disconnected=True)
 
 # calculate mst
 our_mst = mst_prim(g, root_node=5)
-lib_mst = nx.minimum_spanning_tree(g, algorithm='prim')
+lib_mst = nx.minimum_spanning_tree(g, algorithm='kruskal')
 
 # print result
-print(f'Our MST weight: {mst_weight(our_mst)}')
-print(f'Lib MST weight: {mst_weight(lib_mst)}')
+print(f'Our MST: {list(our_mst)}, weight: {mst_weight(our_mst)}')
+print(f'Lib MST: {list(lib_mst)}, weight: {mst_weight(lib_mst)}')
 
 # plot
 plot_graph(g)
